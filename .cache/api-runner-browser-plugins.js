65889084330a2de3ba3c8f734e381e36fa06fd83
Abel Quintero <abel.j.quintero@gmail.com>
2019-02-10 14:56:19 -0800

    adding intial config files

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('/Users/abelquintero/code/portfolio/abel-portfolio/node_modules/gatsby-plugin-offline/gatsby-browser'),
+      options: {"plugins":[]},
+    }]
